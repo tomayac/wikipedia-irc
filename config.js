94a38c54a6db811c01ca43dfd75950d800e15ed9
Thomas Steiner <steiner.thomas@gmail.com>
2013-04-24 16:55:19 +0200

    Typo fix.

--- config.js
+++ config.js
@@ -2,9 +2,14 @@ function readConfig() {
   var fileName = './config.json';
   var text = require("fs").readFileSync(fileName);
   if (!text) {
-    throw new Error('Couldn\'t read config file ' + filename);
+    throw new Error('Couldn\'t read config file ' + fileName);
   }
-  var config = JSON.parse(text);
+  var config;
+  try {
+    config = JSON.parse(text);
+  } catch(e) {
+    throw new Error('The config file is not valid JSON: ' + e);
+  }
   return config;
 }
 
